22039378999698a6f731615cdd8805db69fe0411
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-11 23:34:10 +0530

    firestore package added

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -24,6 +24,8 @@ class ChatViewController: UIViewController {
         title = K.appName
         navigationItem.hidesBackButton = true
         tableView.dataSource = self
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func logOutPressed(_ sender: Any) {
@@ -45,8 +47,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
     
