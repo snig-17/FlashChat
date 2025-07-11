8bc370ce498bdfbfb75c259ecb5937ac13ab3ef5
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-11 13:28:25 +0530

    chat ui

---
+++ FlashChat/Controllers/ChatViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChatViewController.swift
+//  FlashChat
+//
+//  Created by Snigdha Tiwari  on 11/07/2025.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logOutPressed(_ sender: Any) {
+    }
+    
+}
