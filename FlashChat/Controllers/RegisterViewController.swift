577cb1aaf6b9e21876b5bfe923d86685a3c686ed
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-11 13:25:46 +0530

    sign in ui

--- FlashChat/Controllers/RegisterViewController.swift
+++ FlashChat/Controllers/RegisterViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Snigdha Tiwari  on 11/07/2025.
 //
 
-import Foundation
+import UIKit
+
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func registerPressed(_ sender: Any) {
+    }
+    
+}
--- FlashChat/RegisterViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RegisterViewController.swift
-//  FlashChat
-//
-//  Created by Snigdha Tiwari  on 11/07/2025.
-//
-
-import Foundation
