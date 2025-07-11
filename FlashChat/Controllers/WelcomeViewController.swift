253c921f68f89389ebb6c9caa94012abbc67d1f2
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-11 13:22:04 +0530

    register view ui

---
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  FlashChat
+//
+//  Created by Snigdha Tiwari  on 11/07/2025.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = "⚡️FlashChat"
+    }
+
+
+}
+
---
+++ FlashChat/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewController.swift
+//  FlashChat
+//
+//  Created by Snigdha Tiwari  on 11/07/2025.
+//
+
+import Foundation
