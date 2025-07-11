8ab2784421baeb78e40361b20e048c1a2a83e372
snig-17 <snigdha.17.tiwari@icloud.com>
2025-07-11 12:55:05 +0530

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Snigdha Tiwari  on 11/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
